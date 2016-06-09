1512ef40d018f9a3f3d30118e15c84901f10f7ed
Samba5677 <Samba5677@hotmail.com>
2016-06-09 12:06:17 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
-// animate star
-var $star = $('#star');
+var $vcd = ('.vcd');
+var $v = ('.v');
+var $c = ('.c');
+var $d = ('.d');
 
-// DOM element, duration, properties
-TweenMax.to($star, 1, {
-    opacity: 0,
-    x: 180
+TweenMax.from($v, 1, {
+  opacity: 0,
+  ease: Elastic.easeOut.config(15, 0.3), y: 0,
+  scale: 5,
+});
+
+TweenMax.from($c, 1, {
+  opacity: 0,
+  delay: 1,
+  ease: Elastic.easeOut.config(15, 0.3), y: 0,
+  scale: 5,
+});
+
+TweenMax.from($d, 1, {
+  opacity: 0,
+  delay: 2,
+  ease: Elastic.easeOut.config(15, 0.3), y: 0,
+  scale: 5,
 });
